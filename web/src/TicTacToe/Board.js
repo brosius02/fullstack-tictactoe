1a1a8a9ebe80e6e079d01f8f92c4e4ac5633ef3d
JP Dillingham <jpdillingham@gmail.com>
2019-02-28 07:30:58 -0600

    add status message

--- web/src/TicTacToe/Board.js
+++ web/src/TicTacToe/Board.js
@@ -7,12 +7,12 @@ import Cell from './Cell';
 const url = "http://localhost:3001";
 
 class Board extends Component {
-    state = { board: [] };
+    state = { cells: [], gameOver: false, winner: null };
 
     componentDidMount = () => {
         axios.post(url + "/game")
         .then(response => {
-            this.setState({ board: response.data.cells });
+            this.setState(response.data);
         }, err => {
             console.log(err);
         })
@@ -21,30 +21,47 @@ class Board extends Component {
     handleCellClick = (x, y) => {
         axios.post(url + "/turn", { x: x, y: y })
         .then(response => {
-            this.setState({ board: response.data.cells });
+            this.setState(response.data);
         }, err => {
             console.log(err);
         })
-    } 
+    }
+    
+    getGameStatus = (gameOver, winner) => {
+        if (gameOver) {
+            if (winner === 'X' || winner === 'O') {
+                return "Game Over! Winner: " + winner;
+            }
+            else {
+                return "Game Over! It's A Draw!";
+            }
+        }
+        else {
+            return "Game In Progress";
+        }
+    }
 
     render() {
-        let { board } = this.state;
+        let { cells, gameOver, winner } = this.state;
 
         return (
-            <div className="board">
-                {board.map((row, y) => 
-                    <div key={y}>
-                        {row.map((cell, x) => 
-                            <Cell 
-                                key={x+y}
-                                onClick={() => this.handleCellClick(x, y)} 
-                                x={x} 
-                                y={y} 
-                                value={cell}
-                            />
-                        )}
-                    </div>
-                )}
+            <div>
+                <div className="board">
+                    {cells.map((row, y) => 
+                        <div key={y}>
+                            {row.map((cell, x) => 
+                                <Cell 
+                                    key={x+y}
+                                    onClick={() => this.handleCellClick(x, y)} 
+                                    x={x} 
+                                    y={y} 
+                                    value={cell}
+                                />
+                            )}
+                        </div>
+                    )}
+                </div>
+                <span className="status" style={{ color: gameOver ? "grey" : "green" }}>{this.getGameStatus(gameOver, winner)}</span>
             </div>
         )
     }
