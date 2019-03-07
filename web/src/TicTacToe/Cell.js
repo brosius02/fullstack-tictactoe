1cc5ecbc8e85fdf7b17c8ab737a7609ef384e6dd
JP Dillingham <jpdillingham@gmail.com>
2019-03-07 08:03:35 -0600

    add color class to Cell

--- web/src/TicTacToe/Cell.js
+++ web/src/TicTacToe/Cell.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './TicTacToe.css';
 
 const Cell = (props) => (
-    <div className="cell" onClick={props.onClick}>{props.value}</div>
+    <div className={"cell " + props.color} onClick={props.onClick}>{props.value}</div>
 )
 
 export default Cell;
